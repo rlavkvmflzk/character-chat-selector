081e03325855db46db8c1f57ea3cae85e26a9797
rlavkvmflzk <48257409+rlavkvmflzk@users.noreply.github.com>
2024-11-06 05:36:00 +0900

    Add files via upload

--- scripts/module.js
+++ scripts/module.js
@@ -1,20 +1,11 @@
 import { ChatSelector } from './chatSelector.js';
 
-const DEBUG = false;
-
-// 로그용 유틸리티 함수 추가
-const log = (...args) => {
-    if (DEBUG) log(...args);
-};
-
 Hooks.once('init', () => {
-    log('Character Chat Selector | Initializing');
     ChatSelector.initialize();
 });
 
 // 채팅 메시지 생성 전에 추가 처리
 Hooks.on('preCreateChatMessage', (message, data) => {
-    log("Chat message being created with data:", data);
     const select = document.querySelector('.character-select');
     if (select && select.value) {
         const actor = game.actors.get(select.value);
@@ -25,7 +16,6 @@ Hooks.on('preCreateChatMessage', (message, data) => {
                 scene: game.scenes.current?.id,
                 token: null
             };
-            log("Modified chat message data:", data);
         }
     }
     return true;
